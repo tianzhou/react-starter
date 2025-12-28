1bdb3c4785e9b7ef3ba57e0b3bd210d0132cce06
Tianzhou <t@bytebase.com>
2025-12-28 02:04:49 -0800

    feat: add SignUp component

---
+++ src/components/SignUp.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from './AuthLayout';
+
+export default function SignUp() {
+  return (
+    <AuthLayout
+      mode="signup"
+      title="Get Started"
+      description="Create your account to begin"
+    />
+  );
+}
